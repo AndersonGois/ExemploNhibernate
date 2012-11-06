c99965949aa318471836c95d4635f2eae1251953
andersonGois <andersonrejane@hotmail.com>
2012-11-06 20:42:03 -0200

    Cliente

---
+++ ExemploNhibernate/ExemploNhibernate/Domain/Entities/Cliente.cs
@@ -0,0 +1,12 @@
+﻿
+using Domain.Interfaces;
+
+namespace Domain.Entities
+{
+    public  class Cliente :IAggregateRoot<int>
+    {
+        public virtual string Nome { get; set; }
+        public virtual string Descricao { get; set; }
+        public virtual int Id { get; set; }
+    }
+}
